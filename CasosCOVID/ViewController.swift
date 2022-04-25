6809ecf8a567a433c5d187cc75f9fcaca34cc6c3
areyes403 <areyes403>
2022-04-25 09:10:02 -0500

    Initial Commit

---
+++ CasosCOVID/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CasosCOVID
+//
+//  Created by mac17 on 25/04/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
